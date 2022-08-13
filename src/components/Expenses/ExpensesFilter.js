d056835aa0baa3ae5d953eb06a795ce38a9344b5
Nana Kwame <9naquame@gmail.com>
2022-08-13 19:36:01 -0400

    added expenses auxiliaries

---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,22 @@
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+  const handleOnChange = (event) => {
+    props.onChangeExpenseFilter(event.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={handleOnChange}>
+          <option value="2022">2022</option>
+          <option value="2023">2023</option>
+          <option value="2024">2024</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
